9f26d41254abfee3ac002bac169ca22684d515ae
hoodscott <scottdothood@gmail.com>
2015-11-17 22:52:31 +0000

    Adding start and stop buttons
    You can now pretend to start and stop travelling.  Actual functionality
    comes later.

--- www/js/base.js
+++ www/js/base.js
@@ -1,5 +1,11 @@
+var mode = ""
+var running = false
+
 // Function to change the content of green button
 function personal() {
+    // do nothing if we are travelling
+    if (running){}
+    else{
     // get buttons
     var pers = document.getElementById("personal");
     var pub = document.getElementById("public");
@@ -16,13 +22,19 @@ function personal() {
     if ( !pers.classList.contains('per_selected') ){
         // select this button
         pers.classList.add("per_selected");
-        // show start button
+        // set mode
+        mode = "personal"
+        //show startbutton
+        startButtonOn()
     }
     else{
         // otherwise remove selected
         pers.classList.remove('per_selected');
         pub.classList.remove("deselected");
         priv.classList.remove("deselected");
+        // hide start button
+        startButtonOff()
+    }
     }
 }
  
@@ -32,6 +44,9 @@ el_pers.addEventListener("click", function(){personal()}, false);
 
 // Function to change the content of amber
 function public() {
+    // do nothing if we are travelling
+    if (running){}
+    else{
     // get buttons
     var pers = document.getElementById("personal");
     var pub = document.getElementById("public");
@@ -46,8 +61,12 @@ function public() {
     
     // if button is not already selected
     if ( !pub.classList.contains('pub_selected') ){
+        // select this button
         pub.classList.add("pub_selected");
+        // set mode
+        mode = "public"
         // show start button
+        startButtonOn()
     }
     else{
         // otherwise remove selected
@@ -55,6 +74,8 @@ function public() {
         pers.classList.remove("deselected");
         priv.classList.remove("deselected");
         //hide start button
+        startButtonOff()
+    }
     }
 }
  
@@ -64,6 +85,9 @@ el_pub.addEventListener("click", function(){public()}, false);
 
 // Function to change the content of red button
 function private() {
+    // do nothing if we are travelling
+    if (running){}
+    else{
     // get buttons
     var pers = document.getElementById("personal");
     var pub = document.getElementById("public");
@@ -80,16 +104,121 @@ function private() {
     if ( !priv.classList.contains('priv_selected') ){
         // select this button
         priv.classList.add("priv_selected");
+        // set mode
+        mode = "private"
         // show start button
+        startButtonOn()
     }
     else{
         // otherwise remove selected
         priv.classList.remove('priv_selected');
         pub.classList.remove("deselected");
         pers.classList.remove("deselected");
+        startButtonOff()
+    }
     }
 }
  
-// Function to add event listener red button
+// Function to add event listener to red button
 var el_priv = document.getElementById("private");
 el_priv.addEventListener("click", function(){private()}, false);
+
+// functions to show/hide start button
+function startButtonOn(){
+    var start = document.getElementById("start");
+    var help = document.getElementById("please_select");
+    // swap help and start
+    help.classList.add("hidden");
+    start.classList.remove("hidden");
+}
+function startButtonOff(){
+    var start = document.getElementById("start");
+    var help = document.getElementById("please_select");
+    //swap start and help
+    start.classList.add("hidden");
+    help.classList.remove("hidden");
+}
+
+// function to start record of travel
+function start(){
+    running = true
+    var start = document.getElementById("start");
+    var stop = document.getElementById("stop");
+    
+    //swap start and stop
+    start.classList.add("hidden");
+    stop.classList.remove("hidden");
+    
+    // hide other buttons
+    var pers = document.getElementById("personal");
+    var pub = document.getElementById("public");
+    var priv = document.getElementById("private");
+    var pers_h = document.getElementById("personal_blank");
+    var pub_h = document.getElementById("public_blank");
+    var priv_h = document.getElementById("private_blank"); 
+    
+    if (mode!="personal"){
+        pers.classList.add("hidden");
+        pers_h.classList.remove("hidden");
+    }
+    if (mode!="public"){
+        pub.classList.add("hidden");
+        pub_h.classList.remove("hidden");
+    }
+    if (mode!="private"){
+        priv.classList.add("hidden");
+        priv_h.classList.remove("hidden");
+    }
+    
+    //get and store co-ordinates
+    // %todo%
+}
+
+// Function to add event listener to start button
+var el_start = document.getElementById("start");
+el_start.addEventListener("click", function(){start()}, false);
+
+// function to stop record of travel
+function stop(){
+    var start = document.getElementById("start");
+    var stop = document.getElementById("stop");
+    var help = document.getElementById("please_select");
+    
+    // reset variables
+    running = false
+    mode = ""
+    
+    // show popup of stats
+    // %todo%
+    
+    //swap stop and help
+    stop.classList.add("hidden");
+    help.classList.remove("hidden");
+    
+    // reset coloured buttons
+    var pers = document.getElementById("personal");
+    var pub = document.getElementById("public");
+    var priv = document.getElementById("private");
+    var pers_h = document.getElementById("personal_blank");
+    var pub_h = document.getElementById("public_blank");
+    var priv_h = document.getElementById("private_blank"); 
+    // remove selected button
+    pers.classList.remove("per_selected")
+    pub.classList.remove("pub_selected")
+    priv.classList.remove("priv_selected")
+    // un unselect the others
+    pers.classList.remove("deselected")
+    pub.classList.remove("deselected")
+    priv.classList.remove("deselected")
+    // show defaukt buttons
+    pers.classList.remove("hidden");
+    pub.classList.remove("hidden");
+    priv.classList.remove("hidden");
+    // hide blank buttons
+    pers_h.classList.add("hidden");
+    pub_h.classList.add("hidden");
+    priv_h.classList.add("hidden");
+}
+
+var el_stop = document.getElementById("stop");
+el_stop.addEventListener("click", function(){stop()}, false);
