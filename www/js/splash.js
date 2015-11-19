d89257c5574cd4720ec910079ab2a22553147496
hoodscott <scottdothood@gmail.com>
2015-11-19 18:26:27 +0000

    Adding javascript to show a random fact on startup #4
    Also shows the total CO2 produced and clicking anywhere continues you
    through to the app.  Actual facts have still to be added.

--- www/js/splash.js
+++ www/js/splash.js
@@ -1,10 +1,24 @@
+// add a random fact to the homepage
+var facts = ["co2 is bad",
+            "co2 kills trees",
+            "stop using the car",
+            "do  more greens",
+            "eat brussel sprouts"
+            ]
+var rand_fact = Math.floor(Math.random() * 5);      
+var el_facts = document.getElementById('cool_fact');
+el_facts.innerHTML = facts[rand_fact];
+
+// add the user's total emissons to the homepage
+var total_emission = "40kg"
+var el_emission = document.getElementById('total_emissions');
+el_emission.innerHTML = total_emission;
+
+// function to continue to the app
 function advance(){
-    
-    console.log('clicked');
     location.href='base.html';
 }
 
-    console.log('loaded');
-
+// add event listener to whole page to continue to main app
 var el_app = document.getElementById('screen');
 el_app.addEventListener("click", function(){advance()}, false);
